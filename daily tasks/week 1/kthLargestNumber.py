1d89fdf1843a23758864ab3c4ed34847b43352ee
J@tin <ezedin.fedlu@gmail.com>
2021-12-18 18:09:37 +0300

    week-1/kth largest number

--- daily tasks/week 1/kthLargestNumber.py
+++ daily tasks/week 1/kthLargestNumber.py
@@ -1,4 +1,5 @@
 """
+https://leetcode.com/problems/find-the-kth-largest-integer-in-the-array/
 You are given an array of strings nums and an integer k. Each string in nums represents an integer without leading
 zeros.
 
@@ -13,6 +14,7 @@ Explanation:
 The numbers in nums sorted in non-decreasing order are ["3","6","7","10"].
 The 4th largest integer in nums is "3".
 """
+import unittest
 from typing import List
 
 
@@ -22,6 +24,21 @@ def kthLargestNumber(nums: List[str], k: int) -> str:
     return str(nums[-k])
 
 
-print(kthLargestNumber(["3", "6", "7", "10"], 4))
-print(kthLargestNumber(["0", "0"], 2))
-print(kthLargestNumber(["2", "21", "12", "1"], 3))
+class TestSolution(unittest.TestCase):
+    def test_1(self):
+        nums, k = ["3", "6", "7", "10"], 4
+        expected = "3"
+        actual = kthLargestNumber(nums, k)
+        self.assertEqual(expected, actual)
+
+    def test_2(self):
+        nums, k = ["0", "0"], 2
+        expected = "0"
+        actual = kthLargestNumber(nums, k)
+        self.assertEqual(expected, actual)
+
+    def test_3(self):
+        nums, k = ["2", "21", "12", "1"], 3
+        expected = "2"
+        actual = kthLargestNumber(nums, k)
+        self.assertEqual(expected, actual)
