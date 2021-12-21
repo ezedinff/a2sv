536bde6749b4f6ef08e7dbf9cf0447566a6f4c85
J@tin <ezedin.fedlu@gmail.com>
2021-12-21 11:14:07 +0300

    week-2/arithmetic subarrays

--- daily tasks/week 2/arithmetic-subarrays.py
+++ daily tasks/week 2/arithmetic-subarrays.py
@@ -32,13 +32,14 @@ def checkArithmeticSubArrays(nums: List[int], l: List[int], r: List[int]) -> Lis
         if remainder:
             result.append(False)
         else:
-            result.append(all(i in lookup for i in range(min_num, max_num, dividend)))
+            if dividend:
+                result.append(all(i in lookup for i in range(min_num, max_num, dividend)))
     return result
 
 
 class TestSolutions(unittest.TestCase):
     def test_1(self):
-        nums, l, r = [4, 6, 5, 9, 3, 7], [0, 0, 2], [2, 3, 5]
-        expected = [True, False, True]
+        nums, l, r = [-3, -6, -8, -4, -2, -8, -6, 0, 0, 0, 0], [5, 4, 3, 2, 4, 7, 6, 1, 7], [6, 5, 6, 3, 7, 10, 7, 4, 10]
+        expected = [True, True, True, True, False, True, True, True, True]
         actual = checkArithmeticSubArrays(nums, l, r)
         self.assertEqual(expected, actual)
