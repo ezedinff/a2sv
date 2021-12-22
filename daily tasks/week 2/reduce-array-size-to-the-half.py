9b41283c1008d01084cb30adf1cfab5f640a7805
J@tin <ezedin.fedlu@gmail.com>
2021-12-22 14:46:23 +0300

    weel-2/pancake sorting

--- daily tasks/week 2/reduce-array-size-to-the-half.py
+++ daily tasks/week 2/reduce-array-size-to-the-half.py
@@ -14,33 +14,35 @@ which has size 5 (i.e equal to half of the size of the old array). Possible sets
 {3,5},{3,2},{5,2}. Choosing set {2,7} is not possible as it will make the new array [3,3,3,3,5,5,5]
 which has a size greater than half of the size of the old array. """
 import unittest
+from collections import Counter
 from typing import List
 
 
 def minSetSize(arr: List[int]) -> int:
+    # frequencies
     d = {}
     for num in arr:
-        if num in d.keys():
+        if num in d:
             d[num] += 1
         else:
             d[num] = 0
 
-    n = [v for v in d.values()]
-    n.sort()
+    n = sorted(d.values())
 
-    total, ans = 0, 0
-    for i in range(len(n) - 1, 0, -1):
-        if total >= len(arr) // 2:
+    total, ans = len(arr), 0
+    for i in range(len(n)):
+        if total <= len(arr) // 2:
             break
-        total += n[i]
-        ans += 1
+        else:
+            total -= n[i]
+            ans += 1
 
     return ans
 
 
 class TestSolutions(unittest.TestCase):
     def test_1(self):
-        arr = [3, 3, 3, 3, 5, 5, 5, 2, 2, 7]
-        expected = 2
+        arr = [7, 7, 7, 7, 7, 7]
+        expected = 1
         actual = minSetSize(arr)
         self.assertEqual(expected, actual)
