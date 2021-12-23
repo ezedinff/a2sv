6e24d6f9d1f1d8f444c36598ea2401eb68294c8c
J@tin <ezedin.fedlu@gmail.com>
2021-12-23 18:51:08 +0300

    week-2/Reverse Substrings Between Each Pair of Parentheses

---
+++ daily tasks/week 2/reverse-substrings-between-each-pair-of-parentheses.py
@@ -0,0 +1,44 @@
+"""
+
+https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/
+
+You are given a string s that consists of lower case English letters and brackets.
+
+Reverse the strings in each pair of matching parentheses, starting from the innermost one.
+
+Your result should not contain any brackets.
+
+
+Example 1:
+
+Input: s = "(abcd)"
+Output: "dcba"
+
+
+
+Input: s = "(ed(et(oc))el)"
+Output: "leetcode"
+Explanation: First, we reverse the substring "oc", then "etco", and finally, the whole string.
+"""
+
+
+def reverseParentheses(s: str) -> str:
+    stack = []
+    for c in s:
+        if c != ")":
+            stack.append(c)
+        else:
+            temp = ""
+            while stack != [] and stack[-1] != "(":
+                temp += stack.pop()
+
+            stack.pop()
+            stack.append(temp[::-1])
+
+    for i in range(len(stack)):
+        stack[i] = stack[i][::-1]
+
+    return "".join(stack)
+
+
+print(reverseParentheses("(ed(et(oc))el)"))
