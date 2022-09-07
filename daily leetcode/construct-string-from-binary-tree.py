a43e09f5b49b764c9d35d5605534de81a7dfda8b
ezepncl <ezedin.fedlu@pncl.tech>
2022-09-07 10:25:29 +0300

    construct-string-from-binary-tree

--- daily leetcode/construct-string-from-binary-tree.py
+++ daily leetcode/construct-string-from-binary-tree.py
@@ -1,10 +1,3 @@
-#  Construct String from Binary Tree
-'''
-Given the root of a binary tree, construct a string consisting of parenthesis and integers from a binary tree with the preorder traversal way, and return it.
-
-Omit all the empty parenthesis pairs that do not affect the one-to-one mapping relationship between the string and the original binary tree.
-'''
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
