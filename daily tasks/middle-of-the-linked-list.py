2356b412cb8e6e8f6382f2f5499687997078e7b2
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2022-09-23 18:04:56 +0300

    middle of the linked list

--- daily tasks/middle-of-the-linked-list.py
+++ daily tasks/middle-of-the-linked-list.py
@@ -1,9 +1,30 @@
-# Middle of the linked list
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-# https://leetcode.com/problems/middle-of-the-linked-list/
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+        current = head
+        length = 0
+        while current is not None:
+            length += 1
+            current = current.next
+        current = head
+        for i in range(length // 2):
+            current = current.next
+        return current
 
-'''
-Example 1:
-Input: head = [1,2,3,4,5]
-Output: [3,4,5]
-'''
+
+if __name__ == "__main__":
+    head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+    result = Solution().middleNode(head)
+    printList = []
+    expected = [3,4,5]
+    while result is not None:
+        printList.append(result.val)
+        result = result.next
+    
+    assert printList == expected, (printList, expected)
