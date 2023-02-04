b292cf47d0b5911b13bc164feef4d8f391873acc
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2023-02-04 22:13:14 +0300

    74. Search a 2D Matrix

---
+++ 49-progress/search-a-2d-matrix.py
@@ -0,0 +1,35 @@
+# 74. Search a 2D Matrix
+'''
+You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+
+
+'''
+
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            mid_val = matrix[mid // n][mid % n]
+            if mid_val == target:
+                return True
+            elif mid_val < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
