c91ee486241eb1e5a3db246ee5f284580cae2796
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2023-02-09 09:19:23 +0300

    rotate

--- 49-progress/rotate-array.py
+++ 49-progress/rotate-array.py
@@ -1,5 +1,13 @@
+from typing import List 
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         for _ in range(k % len(nums)):
             nums.insert(0, nums.pop())
         return nums
+    
+    # explanation
+    # the idea is to use the built-in insert and pop methods
+    # to rotate the array k times
+    # the modulo operator is used to handle the case where k > len(nums)
+    # the time complexity is O(k * n) and the space complexity is O(1)
